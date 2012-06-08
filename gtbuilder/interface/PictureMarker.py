d60361b9013c8e9c16cba0640591c27f62b8e5c7
Marcus Dillavou <dillavou@uab.edu>
2012-06-08 16:41:31 -0500

    ability to unlink stops from pictures

--- gtbuilder/interface/PictureMarker.py
+++ gtbuilder/interface/PictureMarker.py
@@ -53,17 +53,44 @@ class PictureMarker(Champlain.CustomMarker):
         c = Clutter.Color()
         c.from_string('#FFFFFFEE')
         rect.set_background_color(c)
-        rect.set_size(200, 300)
+        rect.set_size(200, 270)
         rect.set_position(0, 10)
         rect.set_anchor_point(0, 0)
         self.group.add_child(rect)
 
         self.name = Clutter.Text()
-        self.name.set_text('Test Text')
+        self.name.set_text('')
         self.name.set_size(190, 25)
-        self.name.set_position(5, 15)
+        self.name.set_position(20, 15)
         self.name.set_anchor_point(0, 0)
         self.group.add_child(self.name)
+
+        self.info = Clutter.Text()
+        self.info.set_use_markup(True)
+        self.info.set_text('')
+        self.info.set_size(190, 150)
+        self.info.set_position(5, 210)
+        self.info.set_anchor_point(0, 0)
+        self.group.add_child(self.info)
+
+        self.link_stop = Clutter.Text()
+        self.link_stop.set_use_markup(True)
+        self.link_stop.set_markup('<span foreground="blue">+</span>')
+        self.link_stop.set_size(20, 25)
+        self.link_stop.set_position(5, 15)
+        self.link_stop.set_anchor_point(0, 0)
+        self.link_stop.connect('button-release-event', self.on_link_stop)
+        self.group.add_child(self.link_stop)
+
+        self.unlink_stop = Clutter.Text()
+        self.unlink_stop.set_use_markup(True)
+        self.unlink_stop.set_markup('<span foreground="blue">X</span>')
+        self.unlink_stop.set_size(20, 25)
+        self.unlink_stop.set_position(5, 15)
+        self.unlink_stop.set_anchor_point(0, 0)
+        self.unlink_stop.connect('button-release-event', self.on_unlink_stop)
+        self.group.add_child(self.unlink_stop)
+
 
         self.orientation = 0
         try:
@@ -90,6 +117,8 @@ class PictureMarker(Champlain.CustomMarker):
                 (lon.values[2].num /float(lon.values[2].den)) / 3600.0 * j
 
             lat_long = [flat, flon]
+
+            self.info.set_markup('<markup><big><b>GPS Info:</b></big>\n<b>Latitude:</b> %s\n<b>Longitude:</b> %s</markup>' % (lat_long[0], lat_long[1]))
 
             # orientation
             orientation_tag = tags.get('Image Orientation', None)
@@ -121,10 +150,34 @@ class PictureMarker(Champlain.CustomMarker):
 
         self.set_reactive(False)
 
+    def on_link_stop(self, actor, event):
+        print >> sys.stderr, 'Link stop'
+        return False
+    def on_unlink_stop(self, actor, event):
+        print >> sys.stderr, 'Unlink stop'
+        if self.picture.stop:
+            self.picture.stop = None
+
+        # update our text
+        self.on_click(None, None)
+        self.on_click(None, None)
+
+        return False
+
     def on_click(self, actor, event):
         self._visible = not self._visible
 
+        # disable somethings
+        self.link_stop.set_reactive(False)
+        self.unlink_stop.set_reactive(False)
+
         if self._visible:
+            # update our name
+            if self.picture.stop:
+                self.name.set_text('%s) Stop: (%s) %s' % (self.picture.picture_id, self.picture.stop.stop_id, self.picture.stop.name))
+            else:
+                self.name.set_text('(%s) No Stop Linked' % self.picture.picture_id)
+
             # create our texture
             try:
                 self.picture_box = Clutter.Texture()
@@ -135,7 +188,7 @@ class PictureMarker(Champlain.CustomMarker):
                 self.picture_box.set_keep_aspect_ratio(True)
                 #self.picture_box.set_size(150, 150)
                 self.picture_box.set_width(150)
-                self.picture_box.set_position(25, 50)
+                self.picture_box.set_position(25, 60)
                 self.picture_box.set_anchor_point(0, 0)
                 self.picture_box.set_z_rotation_from_gravity(self.orientation, Clutter.Gravity.CENTER)
                 self.group.add_child(self.picture_box)
@@ -144,6 +197,13 @@ class PictureMarker(Champlain.CustomMarker):
                 #!mwd - load a broken image instead?
 
             self.group.show_all()
+
+            if self.picture.stop:
+                self.link_stop.hide()
+                self.unlink_stop.set_reactive(True)
+            else:
+                self.unlink_stop.hide()
+                self.link_stop.set_reactive(True)
         else:
             self.group.hide_all()
             self.group.remove_child(self.picture_box)
