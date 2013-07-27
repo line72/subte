909c2b8d5630c4b1aca9139a874b1cca8edf7935
Marcus Dillavou <dillavou@vipaar.com>
2013-07-27 11:08:29 -0500

    Restore the next blocks in the gui

--- libsubte/interface/TripList.py
+++ libsubte/interface/TripList.py
@@ -116,7 +116,27 @@ class TripList(Gtk.VBox):
             for j, s in enumerate(self._trip_route.stops):
                 ts = t.stops[j]
                 trip.append(ts.arrival)
-            trip.append('')
+
+            # see if there is a next block
+            if t.next_block:
+                it = self.next_block_model.get_iter_first()
+                done = False
+                while it and not done:
+                    cols = [0, 1]
+                    values = self.next_block_model.get(it, *cols)
+
+                    if values[0] == t.next_block.trip_id:
+                        trip.append(values[1])
+                        done = True
+
+                    it = self.next_block_model.iter_next(it)
+
+                if not done:
+                    print 'Unable to find a match for the next stop. This must be a missing reference'
+                    trip.append('') # didn't find a match
+            else:
+                trip.append('')
+
             self.model.append(trip)
 
     def clear_model(self):
